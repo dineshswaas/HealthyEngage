e9e5f09579649f824c55dd391f31cdfaf5a8fb54
dinesh <dineshkumar.k@swaas.net>
2019-11-04 21:48:58 +0530

    connect added

--- HealthyEngage/app/src/main/java/com/swaas/healthyengage/AssessmentDetailsActivity.java
+++ HealthyEngage/app/src/main/java/com/swaas/healthyengage/AssessmentDetailsActivity.java
@@ -1,9 +1,12 @@
 package com.swaas.healthyengage;
 
 
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -14,30 +17,42 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.warkiz.tickseekbar.TextPosition;
+import com.warkiz.tickseekbar.TickMarkType;
+import com.warkiz.tickseekbar.TickSeekBar;
+import com.zhouyou.view.seekbar.SignSeekBar;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import models.CarePlanModels;
 import utils.Constants;
 
+import static java.security.AccessController.getContext;
+
 public class AssessmentDetailsActivity extends AppCompatActivity {
 
 
-    TextView name,description,question,booleanquestion,pickerquestion;
+    TextView name,description,question,booleanquestion,pickerquestion,sliderquestion,minlabel,maxlabel;
     CarePlanModels.CarePlanAssessment carePlanAssessment;
-    RelativeLayout textParentLayout,booleanParentView,pickerParentView;
+    RelativeLayout textParentLayout,booleanParentView,pickerParentView,sliderParentView,buttonlayout;
     EditText textEditText;
     RadioGroup radioGroup;
     RadioButton rpositiveButton,rnegativeButton;
     Spinner pickerSpinner;
     List<String> pickerList;
-
+    TickSeekBar  tickSeekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_details);
-        initializeView();
-        getIntentData();
+        try {
+            initializeView();
+            getIntentData();
+        }catch (Exception e){
+            Log.d("parm",e.getMessage());
+        }
+
 
     }
 
@@ -60,22 +75,64 @@ public class AssessmentDetailsActivity extends AppCompatActivity {
             textParentLayout.setVisibility(View.VISIBLE);
             booleanParentView.setVisibility(View.GONE);
             pickerParentView.setVisibility(View.GONE);
+            sliderParentView.setVisibility(View.GONE);
             onBindTextDetails();
         }else if(carePlanAssessment.getInput_type().equalsIgnoreCase(Constants.INPUT_BOOLEAN)){
             textParentLayout.setVisibility(View.GONE);
             booleanParentView.setVisibility(View.VISIBLE);
             pickerParentView.setVisibility(View.GONE);
+            sliderParentView.setVisibility(View.GONE);
             onBindBooleanDetails();
         }else if(carePlanAssessment.getInput_type().equalsIgnoreCase(Constants.INPUT_PICKER)){
             textParentLayout.setVisibility(View.GONE);
             booleanParentView.setVisibility(View.GONE);
             pickerParentView.setVisibility(View.VISIBLE);
+            sliderParentView.setVisibility(View.GONE);
             onBindPickerDetails();
         }else if(carePlanAssessment.getInput_type().equalsIgnoreCase(Constants.INPUT_SLIDER)){
             textParentLayout.setVisibility(View.GONE);
             pickerParentView.setVisibility(View.GONE);
             booleanParentView.setVisibility(View.GONE);
+            sliderParentView.setVisibility(View.VISIBLE);
+            onBindSliderDetails();
         }
+
+    }
+
+    private void onBindSliderDetails() {
+
+        tickSeekBar = TickSeekBar
+                .with(this)
+                .max(200)
+                .min(10.2f)
+                .progressValueFloat(true)
+                .progress(33)
+                .tickCount(Integer.parseInt(carePlanAssessment.getMax()) + 1)
+                .showTickMarksType(TickMarkType.DIVIDER)
+                .tickMarksColor(getResources().getColor(R.color.blue_non_pressed))
+                .tickMarksSize(6)//dp
+                .tickTextsSize(13)//sp
+                .showTickTextsPosition(TextPosition.ABOVE)
+                .tickTextsColorStateList(getResources().getColorStateList(R.color.black))
+                .thumbColor(Color.parseColor("#ff0000"))
+                .thumbSize(14)
+                .trackProgressColor(getResources().getColor(R.color.interventionblue))
+                .trackProgressSize(4)
+                .trackBackgroundColor(getResources().getColor(R.color.draft_grey))
+                .trackBackgroundSize(2)
+                .build();
+
+        if(!TextUtils.isEmpty(carePlanAssessment.getQuestion())){
+            sliderquestion.setText(carePlanAssessment.getQuestion());
+        }
+
+        if(!TextUtils.isEmpty(carePlanAssessment.getLabel_min())){
+            minlabel.setText(carePlanAssessment.getLabel_min());
+        }
+        if(!TextUtils.isEmpty(carePlanAssessment.getLabel_max())){
+            maxlabel.setText(carePlanAssessment.getLabel_max());
+        }
+
 
     }
 
@@ -150,6 +207,19 @@ public class AssessmentDetailsActivity extends AppCompatActivity {
         pickerParentView = (RelativeLayout)findViewById(R.id.pickerParentView);
         pickerSpinner= (Spinner)findViewById(R.id.pickerSpinner);
         pickerquestion = (TextView)findViewById(R.id.pickerquestion);
+        sliderParentView = (RelativeLayout)findViewById(R.id.sliderParentView);
+        sliderquestion = (TextView)findViewById(R.id.sliderquestion);
+        tickSeekBar = (TickSeekBar) findViewById(R.id.tickSeekBar);
+        minlabel = (TextView)findViewById(R.id.minlabel);
+        maxlabel = (TextView)findViewById(R.id.maxlabel);
+        buttonlayout = (RelativeLayout)findViewById(R.id.buttonlayout);
+
+        buttonlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 
