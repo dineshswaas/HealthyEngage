10dcd17df44bce1e490a223495665bf17af67c10
dinesh <dineshkumar.k@swaas.net>
2019-11-06 11:59:51 +0530

    connect added

---
+++ HealthyEngage/app/src/main/java/com/swaas/healthyengage/GlobalApplication.java
@@ -0,0 +1,27 @@
+package com.swaas.healthyengage;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+
+public class GlobalApplication extends Application {
+
+
+
+public static GlobalApplication sThis;
+    protected String userAgent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sThis = this;
+
+        Stetho.initializeWithDefaults(this);
+
+    }
+
+    public static GlobalApplication getThis() {
+        return sThis;
+    }
+}
